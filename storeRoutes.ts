e3674c50262e8f981b0d6870682e3bb91bf76b92
Sakshi Sachin Kalyankar <sakshikalyankar123@gmail.com>
2025-09-19 18:23:41 +0530

    Delete storeRoutes.ts

--- storeRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express = require('express');
-import { getStores, addStore } from '../controllers/storeController';
-import { protect, admin } from '../middleware/authMiddleware';
-
-const router = express.Router();
-
-router.route('/').get(protect, getStores).post(protect, admin, addStore);
-
-export default router;
